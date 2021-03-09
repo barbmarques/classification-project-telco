3489b0e11e9440d77654272f2fa17301bf229343
Barb Marques <barbara.ann.marques@gmail.com>
2021-03-08 23:02:58 -0600

    Additional revisons to modeling on validate and test data sets

--- README.md
+++ README.md
@@ -12,7 +12,41 @@ Database of 7043 customers, 24 features
 - Find drivers for customer churn at Telco
 - Construct a ML classification model that accurately predicts customer churn.
 
-###Insert Data Dictionary Here
+
+### Data Dictionary
+
+|Variable | Definition | Data Type | Value Counts |
+|
+|churn|This is our target **variable** Indicates that a customer has cancelled servicer| object |
+
+
+---
+| Attribute | Definition | Data Type |
+| ----- | ----- | ----- |
+|payment\_type\_id | Indicates how a customer pays their bill each month | int64 |
+|contract\_type\_id| Indicates which contract type a customer has | int64 |
+|internet\_service\_type_id| Indicates what type of internet service a customer has | int64 |
+|customer\_id|Alpha-numeric ID that identifies each customer| object |
+gender|Gender of the customer| object |
+senior_citizen|Indicates if the customer is 65 or older| int64 |
+partner|If a customer is married| object | 
+dependents|Indicates if a customer lives with dependents| object |
+tenure|The length of a customers relationship with Telco™ measured in months|  int64 |
+phone_service|If a customer has phone service| object |
+multiple_lines|If a customer has multiple phone lines| object |
+online_security|Indicates if a customer has online security add-on| object |
+online_backup|Indicates if a customer has online backups add-on| object |
+device_protection|Indicates if a customer has a protection plan for Telco™ devices| object |
+tech_support|Indicates whether a customer has technical support add-on| object |
+streaming_tv|Indicates if a customer uses internet to stream tv| object |
+streaming_movies|Indicates if a customer uses internet to stream movies| object |
+paperless_billing|Indicates if a customer is enrolled in paperless billing| object |
+monthly_charges|The amount a customer pays each month for services with Telco™| object |
+total_charges|The total amount a customer has paid for Telco™ services| object |
+|internet\_service\_type|Indicates the type of internet service a customer has| object |
+|contract_type|The type of contract a customer has| object |
+|payment_type|How a customer pays their bill each month| object |
+
 
 
 
--- prepare.py
+++ prepare.py
@@ -30,6 +30,16 @@ def clean_telco(df):
     df = df.rename(columns = {'tenure':'tenure_months'})
     df['tenure_years'] = round(df.tenure_months / 12, 2)
 
+    # Drop gender (exporation shows it's nearly evenly split and therefore not likely a driver of churn)
+    df.drop(columns = ['gender'])
+    
+    # Encode booleans
+    # Create one variable 'phone_services' combining phone service and multiple lines
+    # 0 = no phone service, 1 = one line, 2 = multiple lines 
+    # delete original phone_service and multiple_lines variables
+    df['phone_services'] = df.multiple_lines.replace({'No phone service': 0, 'No': 1, 'Yes': 2})
+    df.drop(columns=['phone_service', 'multiple_lines'], inplace = True)    
+    
     # Creating dataframe of dummy variables for gender: male = 1, female = 0, streaming_tv, and streaming_movies
     df_dummies = pd.get_dummies(df[['gender', 'streaming_tv', 'streaming_movies']], drop_first = False)
 
@@ -40,37 +50,35 @@ def clean_telco(df):
     dummycols = ['gender', 'streaming_tv', 'streaming_movies']
     df = df.drop(columns = dummycols, axis = 1)
     
-    # Encode booleans
+    # combine two columns streaming_tv and streaming_movies
+    df['streamer'] = (((df['streaming_tv_Yes'] == 1) & (df['streaming_movies_Yes'] == 1)) | ((df['streaming_tv_Yes'] == 1) | (df['streaming_movies_Yes'] == 1))) 
     
-    
-    # Create one variable 'phone_services' combining phone service and multiple lines
-    # 0 = no phone service, 1 = one line, 2 = multiple lines 
-    # delete original phone_service and multiple_lines variables
-    df['phone_services'] = df.multiple_lines.replace({'No phone service': 0, 'No': 1, 'Yes': 2})
-    df.drop(columns=['phone_service', 'multiple_lines'], inplace = True)    
+    # drop original columns that were combined
+    df.drop(columns=['streaming_tv_No','streaming_tv_Yes','streaming_tv_No internet service','streaming_movies_No','streaming_movies_Yes','streaming_movies_No internet service'], inplace = True)
     
     # Manually encode yes/no data to 0 = no, 1 - yes
-    df['churn'] = df['churn'].replace({'No' : 0, 'Yes' : 1})
+    df['has_churned'] = df['churn'].replace({'No' : 0, 'Yes' : 1})
     df['partner'] = df['partner'].replace({'No' : 0, 'Yes' : 1})
     df['dependents'] = df['dependents'].replace({'No' : 0, 'Yes' : 1})
     df['paperless_billing'] = df['paperless_billing'].replace({'No': 0, 'Yes': 1})
     
-    # Create new columns for partner/dependents combinations
-    df['single'] = (df['partner'] == 0) & (df['dependents'] == 0)
-    df['partner_no_dep'] = (df['partner'] == 1) & (df['dependents'] == 0)
-    df['single_with_dep'] = (df['partner'] == 0) & (df['dependents'] == 1)
+    # Create new columns for partner/dependents combinations & drop original columns
+    df['is_single_no_dep'] = (df['partner'] == 0) & (df['dependents'] == 0)
+    df['has_partner_no_dep'] = (df['partner'] == 1) & (df['dependents'] == 0)
+    df['is_single_with_dep'] = (df['partner'] == 0) & (df['dependents'] == 1)
     df['family'] = (df['partner'] == 1) & (df['dependents'] == 1)
+    df.drop(columns=['partner', 'dependents'], inplace = True)   
             
-   # Create new columns for contract types 
+    # Create new columns for contract types 
     df['month_to_month'] = (df['contract_type_id'] == 1)
     df['one_year'] = (df['contract_type_id'] == 2) 
     df['two_year'] = (df['contract_type_id'] == 3) 
     df.drop(columns=['contract_type_id'], inplace = True)   
                    
    # Create new columns for payment types 
-    df['e_Check'] = (df['payment_type_id'] == 1)
-    df['check'] = (df['payment_type_id'] == 2) 
-    df['auto_pay'] = (df['payment_type_id'] == 3) | (df['payment_type_id'] == 4)
+    df['use_Check'] = (df['payment_type_id'] == 1)
+    df['sends_check'] = (df['payment_type_id'] == 2) 
+    df['has_auto_pay'] = (df['payment_type_id'] == 3) | (df['payment_type_id'] == 4)
     df.drop(columns=['payment_type_id'], inplace = True)   
     
     # Create new columns for internet service types 
@@ -78,14 +86,15 @@ def clean_telco(df):
     df['fiber'] = (df['internet_service_type_id'] == 2) 
     df['no_internet'] = (df['internet_service_type_id'] == 3) 
     df.drop(columns=['internet_service_type_id'], inplace = True)  
-                           
-        
+                                  
     # Manually encode yes/no/no service data to 0 = no (includes those with no service), 1 - yes
-    df['tech_support'] = df.tech_support.replace({'No': 0, 'Yes': 1, 'No internet service': 0})
-    df['online_security'] = df.online_security.replace({'No': 0, 'Yes': 1, 'No internet service': 0})
-    df['online_backup'] = df.online_backup.replace({'No': 0, 'Yes': 1, 'No internet service': 0})
-    df['device_protection'] = df.device_protection.replace({'No': 0, 'Yes': 1, 'No internet service': 0})
+    df['no_tech_support'] = df.tech_support.replace({'No': 1, 'Yes': 0, 'No internet service': 0})
+    df['no_online_security'] = df.online_security.replace({'No': 1, 'Yes': 0, 'No internet service': 0})
+    df['no_online_backup'] = df.online_backup.replace({'No': 1, 'Yes': 0, 'No internet service': 0})
+    df['no_device_protection'] = df.device_protection.replace({'No': 1, 'Yes': 0, 'No internet service': 0})
     
+    # drop original columns that were encoded in the negative above
+    df.drop(columns=['tech_support','online_security','online_backup','device_protection'], inplace = True)
     
     # Change bool columns to False: 0, True: 1
     u = df.select_dtypes(bool)
